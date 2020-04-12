6eb3e4ffdc4f38786eafb079ec6ffbdca72c0cde
VCM admin account <vcm@vcm-12423.vm.duke.edu>
2020-04-12 12:31:40 -0400

    migrate

--- docker-deploy/web-app/stock/models.py
+++ docker-deploy/web-app/stock/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class stock(models.Model):
     pid = models.IntegerField(default=-1) # product id
-    description = models.CharField(default="") # product description
+    description = models.CharField(max_length=1000, default="") # product description
     count = models.IntegerField(default=-1) # product quantity
     worldid = models.IntegerField(default=-1)  # world id
     whnum = models.IntegerField(default=-1)  # warehouse id
