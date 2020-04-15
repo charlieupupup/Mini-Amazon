bea6b89bb2022c86b233b287b9330f798b841d16
Cheng Peng <cheng@satorianalytics.com>
2020-04-15 17:08:24 -0400

    order

--- docker-deploy/web-app/order/models.py
+++ docker-deploy/web-app/order/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class order(models.Model):
+    # each record: one kind of products in one package
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # user
+    x = models.IntegerField(default=-1)  # user address
+    y = models.IntegerField(default=-1)  # user address
+    pkgid = models.IntegerField(default=-1) # package id & Tracking num & Ship id
+    pid = models.IntegerField(default=-1)  # product id
+    count = models.IntegerField(default=-1)  # product quantity
--- docker-deploy/web-app/stock/models.py
+++ docker-deploy/web-app/stock/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class stock(models.Model):
+    # each record: one kind of products in one warehouse
     pid = models.IntegerField(default=-1) # product id
     description = models.CharField(max_length=1000, default="") # product description
     count = models.IntegerField(default=-1) # product quantity
