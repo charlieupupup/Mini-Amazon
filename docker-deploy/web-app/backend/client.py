f85f98665f2d024de7805c7a0081690febbc7752
guanhua <charlieupupup@gmail.com>
2020-04-18 23:19:58 -0400

    base framework for backend

---
+++ docker-deploy/web-app/backend/client.py
@@ -0,0 +1,62 @@
+import socket
+import io
+#encode & decode
+from google.protobuf.internal.decoder import _DecodeVarint32
+from google.protobuf.internal.encoder import _VarintBytes
+
+# allow connection from all host
+HOST = ''
+PORT = 23456
+
+"""
+send request to world & ups
+"""
+
+
+class Client():
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.world_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def send(self, msg):
+        data_string = msg.SerializeToString()
+        size = msg.ByteSize()
+        self.sock.sendall(_VarintBytes(size))
+        self.sock.sendall(data_string)
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            s.sendall(b'Hello, world')
+            data = s.recv(1024)
+
+    def recv(self):
+        # Brian
+        var_int_buff = []
+
+        while True:
+            buf = world_socket.recv(1)
+            var_int_buff += buf
+            msg_len, new_pos = _DecodeVarint32(var_int_buff, 0)
+            if new_pos != 0:
+                break
+        whole_message = world_socket.recv(msg_len)
+        # int length is at most 4 bytes long
+        hdr_bytes = self.sock.recv(4)
+        (msg_length, hdr_length) = _DecodeVarint32(hdr_bytes, 0)
+        rsp_buffer = io.BytesIO()
+        if hdr_length < 4:
+            rsp_buffer.write(hdr_bytes[hdr_length:])
+
+        # read the remaining message bytes
+        msg_length = msg_length - (4 - hdr_length)
+        while msg_length > 0:
+            rsp_bytes = self.sock.recv(min(8096, msg_length))
+            rsp_buffer.write(rsp_bytes)
+            msg_length = msg_length - len(rsp_bytes)
+
+        return rsp_buffer.getvalue()
+
+    """
+    proceed the amazon command
+    """
