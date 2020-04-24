206b6771cc3a10a6590369e34a7ec3a1ab276941
Cheng Peng <cheng@satorianalytics.com>
2020-04-24 13:47:51 -0400

    msg

--- docker-deploy/web-app/backend/ups.py
+++ docker-deploy/web-app/backend/ups.py
@@ -19,6 +19,7 @@ class UPS(Base):
         # send back
         msg_init = IG1_pb2.AMsg()
         msg_init.acks.append(msg.initworld.seq)
+        print(msg_init)
         self.send(msg_init)
         # tell world
         self.world.init(msg.initworld.worldid)
